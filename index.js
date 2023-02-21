366cf97c9686b13b751619f4fa8a4936147cb5de
Nguyễn Hải <106507349+Nguyenhai06@users.noreply.github.com>
2023-02-21 02:28:08 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="style.css">
+      <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+      <title>Together</title>
+</head>
+<body>
+     <div id="wrapper">
+           <h1>Bên Nhau</h1>
+
+           <div id="clock-box">
+                 <div id="clock">
+                       <date>0 Ngày</date>
+                       <time>00:00:00</time>
+                  </div>
+            </div>
+            
+            <div id="info">
+                  <div class="one">
+                        <img src="img/male.jpg" class="avt">
+                        <p>Nguyễn Hải</p>
+                  </div>
+                  <div id="heart">
+                        ❤<anni>...</anni>
+                  </div>
+                  <div class="two">
+                        <img src="img/female.jpg" class="avt">
+
+                        <p>Khánh Linh</p>
+                  </div>
+            </div>
+            
+            <div id="music">
+                  <audio loop autoplay controls>Your browser does not support the audio element.</audio>
+            </div>
+
+            <footer>💕 Cảm ơn vì khoảng thời gian ta bên nhau 💕</footer>
+      </div> 
+      
+      <script src="app.js"></script>
+</body>
+</html>
